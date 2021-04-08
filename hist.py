1b97c36ebf3207cd09242f7b08743bdfd924db65
Tatiana Frayman <alekseilier@ya.ru>
2021-04-08 20:04:31 +0300

    гистограмма изображения

---
+++ hist.py
@@ -0,0 +1,12 @@
+import numpy as np
+import cv2
+from matplotlib import pyplot as plt
+
+img = cv2.imread("C:/Users/asus/Desktop/girl.jpg", 0)
+hist = cv2.calcHist([img], [0], None, [256], [0,256])
+
+plt.plot(hist)
+plt.show()
+
+plt.hist(img.ravel(), 256, [0,256])
+plt.show()
